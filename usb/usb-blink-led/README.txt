95d87f87fb0e9743ea50c49dbb7b5b91b693cd4d
Amitesh Singh <singh.amitesh@gmail.com>
2016-08-28 13:46:48 +0530

    Add proper usb blink led driver

---
+++ usb/usb-blink-led/README.txt
@@ -0,0 +1,1 @@
+This driver is for avr based led module
---
+++ usb/usb-blink-led/blink-led.c
@@ -0,0 +1,145 @@
+#include <linux/init.h>// macros used to markup functions e.g. __init, __exit
+#include <linux/module.h>// Core header for loading LKMs into the kernel
+#include <linux/kernel.h>// Contains types, macros, functions for the kernel
+#include <linux/device.h>// Header to support the kernel Driver Model
+
+#include <linux/usb.h> //for usb stuffs
+#include <linux/slab.h> //for
+
+#include <linux/kthread.h> // for kthread
+#include <linux/delay.h> //for sleep
+#include <linux/signal.h> //for allow_signal
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Amitesh Singh");
+MODULE_DESCRIPTION("first usb driver module");
+MODULE_VERSION("0.1"); // --> module version of kernel driver
+
+
+//one structure for each connected device
+struct my_usb {
+     struct usb_device *udev;
+     struct task_struct *task; //kthread ptr
+};
+
+#define MY_USB_VENDOR_ID 0x16c0
+#define MY_USB_PRODUCT_ID 0x03e8
+static struct usb_device_id my_usb_table[] = {
+       { USB_DEVICE(MY_USB_VENDOR_ID, MY_USB_PRODUCT_ID) },
+       {},
+};
+
+MODULE_DEVICE_TABLE(usb, my_usb_table);
+
+
+static int
+blink_led_func(void *data)
+{
+   struct my_usb *sd = data;
+   int err;
+   uint8_t value = 0;
+
+   printk(KERN_ALERT "worker func is called");
+
+   allow_signal(SIGKILL);
+   while (!kthread_should_stop())
+     {
+        value ^= 0x01;
+        printk(KERN_INFO "sending control msg: %d", value);
+        err = usb_control_msg(sd->udev,
+                              usb_sndctrlpipe(sd->udev, 0),
+                              value, USB_TYPE_VENDOR | USB_DIR_OUT,
+                              0, 0,
+                              NULL, 0,
+                              1000);
+        ssleep(10);
+     }
+
+   return 0;
+}
+
+//called when a usb device is connected to PC
+static int
+my_usb_probe(struct usb_interface *interface,
+             const struct usb_device_id *id)
+{
+   struct usb_device *udev = interface_to_usbdev(interface);
+   struct my_usb *data;
+
+   printk(KERN_INFO "manufacturer: %s", udev->manufacturer);
+   printk(KERN_INFO "product: %s", udev->product);
+
+   data = kzalloc(sizeof(struct my_usb), GFP_KERNEL);
+   if (data == NULL)
+     {
+        //handle error
+     }
+
+   //increase ref count, make sure u call usb_put_dev() in disconnect()
+   data->udev = usb_get_dev(udev);
+   usb_set_intfdata(interface, data);
+
+   printk(KERN_INFO "usb device is connected");
+
+   data->task = kthread_run(&blink_led_func, data, "_blink_led_");
+   printk(KERN_INFO "started thread: %s", data->task->comm);
+
+   return 0;
+}
+
+//called when unplugging a USB device
+static void
+my_usb_disconnect(struct usb_interface *interface)
+{
+   struct my_usb *data;
+
+   data = usb_get_intfdata(interface);
+   kthread_stop(data->task);
+
+   usb_set_intfdata(interface, NULL);
+
+   //deref the count
+   usb_put_dev(data->udev);
+
+   kfree(data); //deallocate, allocated by kzmalloc()
+
+   printk(KERN_INFO "usb device is disconnected");
+}
+
+static struct usb_driver my_usb_driver = {
+     .name = "my first usb driver",
+     .id_table = my_usb_table,
+     .probe = my_usb_probe,
+     .disconnect = my_usb_disconnect,
+};
+
+//called on module loading
+static int __init
+_usb_init(void)
+{
+   int result;
+   printk(KERN_INFO "usb driver is loaded");
+
+   result = usb_register(&my_usb_driver);
+   if (result)
+     {
+        printk(KERN_ALERT "device registeration failed!!");
+     }
+   else
+     {
+        printk(KERN_INFO "device registered");
+     }
+
+   return result;
+}
+
+//called on module unloading
+static void __exit
+_usb_exit(void)
+{
+   printk(KERN_INFO "usb driver is unloaded");
+   usb_deregister(&my_usb_driver);
+}
+
+module_init(_usb_init);
+module_exit(_usb_exit);
