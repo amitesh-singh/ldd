d8b7aefa2b84e0725b17efa0f3cebb975d6c3c7a
Amitesh Singh <singh.amitesh@gmail.com>
2016-12-17 12:19:29 +0530

    remove unused macros.

--- tfts/tftframework/st7735r.c
+++ tfts/tftframework/st7735r.c
@@ -13,19 +13,8 @@
 #include <linux/vmalloc.h>
 
 #include "common.h"
-#define ST7735_COLOR(red, green, blue)   ((unsigned int)( (( blue >> 3 ) << 11 ) | (( green >> 2 ) << 5  ) |  ( red  >> 3 )))
 
-#define BLACK                       ST7735_COLOR(0x00, 0x00, 0x00)
-#define WHITE                       ST7735_COLOR(0xFF, 0xFF, 0xFF)
-#define RED                         ST7735_COLOR(0xFF, 0x00, 0x00)
-#define GREEN                       ST7735_COLOR(0x00, 0xFF, 0x00)
-#define BLUE                        ST7735_COLOR(0x00, 0x00, 0xFF)
-#define YELLOW                      ST7735_COLOR(0xFF, 0xFF, 0x00)
-#define MAGENTA                     ST7735_COLOR(0xFF, 0x00, 0xFF)
-#define CYAN                        ST7735_COLOR(0x00, 0xFF, 0xFF)
-#define GRAY                        ST7735_COLOR(0x80, 0x80, 0x40)
-#define SILVER                      ST7735_COLOR(0xA0, 0xA0, 0x80)
-#define GOLD                        ST7735_COLOR(0xA0, 0xA0, 0x40)
+
 #define ST7735_NOP         (0x0)
 #define ST7735_SWRESET     (0x01)
 #define ST7735_SLPIN       (0x10)
