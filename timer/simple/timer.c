e82b2c434ee0a3f52821a4f96428664e2f7dce3b
Amitesh Singh <singh.amitesh@gmail.com>
2019-01-22 22:43:50 +0530

    fix the timer code according to new linux kernel. 4.19 - arch linux

--- timer/simple/timer.c
+++ timer/simple/timer.c
@@ -1,6 +1,7 @@
 #include <linux/init.h> // macros used to markup functions e.g. __init, __exit
 #include <linux/module.h> // Core header for loading LKMs into the kernel
 #include <linux/kernel.h> // Contains types, macros, functions for the kernel
+#include <linux/timer.h> // for timer and timer related functions
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Amitesh Singh");
@@ -9,24 +10,25 @@ MODULE_VERSION("0.1");
 
 //jiffies is the current time.
 static struct timer_list timer_1;
-static unsigned advance = 100;
 
 static void
-timer_timeout(unsigned long x)
+timer_timeout(struct timer_list *timer)
 {
    printk(KERN_ALERT "timer is called");
    //keep calling timer_1 on every 100 ms
-   timer_1.expires = jiffies + msecs_to_jiffies(100);
-   add_timer(&timer_1);
+   //timer_1.expires = jiffies + msecs_to_jiffies(100);
+   //add_timer(&timer_1);
+   mod_timer(&timer_1, jiffies + msecs_to_jiffies(100));
 }
 
 static int __init
 _timer_init(void)
 {
-   init_timer(&timer_1);
-   timer_1.function = timer_timeout;
-   timer_1.expires = jiffies + msecs_to_jiffies(advance);
-   add_timer(&timer_1);
+   //for first time use
+   // or timer_setup
+   timer_setup_on_stack(&timer_1, timer_timeout, 0);
+
+   mod_timer(&timer_1, jiffies + msecs_to_jiffies(100));
    printk(KERN_INFO "started timer..");
 
    return 0;
@@ -35,6 +37,7 @@ _timer_init(void)
 static void
 _timer_exit(void)
 {
+   destroy_timer_on_stack(&timer_1);
    del_timer(&timer_1);
    printk(KERN_INFO "exit timer..");
 }
