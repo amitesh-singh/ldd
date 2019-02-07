9a41ebcadc341018072ad8960393f7e9b9c13fe0
Amitesh Singh <singh.amitesh@gmail.com>
2019-02-07 17:04:15 +0530

    use pr_info

--- generic/hello/hello-1.c
+++ generic/hello/hello-1.c
@@ -6,13 +6,19 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Amitesh Singh");
 MODULE_DESCRIPTION("A Hello Ami module");
 
-static int hello_init(void) {
-  printk(KERN_INFO " Hello ami!\n");
-  return 0;
+static int __init hello_init(void)
+{
+   //printk(KERN_INFO " Hello ami!\n");
+   //pr_info is defined as printk(KERN_INFO..), pr_alert() also can be used instead of
+   // printk(KERN_ALERT ..);
+   pr_info(" Hello Ami!");
+   return 0;
 }
 
-static void hello_exit(void) {
-  printk(KERN_INFO "Bye, ami\n");
+static void __exit hello_exit(void)
+{
+   //printk(KERN_INFO "Bye, ami\n");
+   pr_info(" Bye Ami!");
 }
 
 module_init(hello_init);
