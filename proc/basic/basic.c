afb6d66230ce9b45c3dd2ace49169e082f780d37
Amitesh Singh <singh.amitesh@gmail.com>
2022-05-08 20:26:05 +0530

    add proc fs example

--- proc/basic/basic.c
+++ proc/basic/basic.c
@@ -5,97 +5,44 @@
 #include <linux/mm.h>
 #include <linux/slab.h> //kmalloc
 
-#define DATA_SIZE 1024
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Amitesh Singh");
 
-char *msg = NULL;
-struct proc_dir_entry *proc;
-int len;
+#define BUFSIZE  100
 
+static struct proc_dir_entry *ent;
 
-ssize_t my_proc_read(struct file *filp, char *buf, size_t count, loff_t *offp)
+static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos)
 {
-   int err;
-
-   char *data = PDE_DATA(file_inode(filp));
-   if ((int)(*offp) > len)
-     return 0;
-
-   if (!data)
-     {
-        printk(KERN_ALERT "NULL DATA");
-        return 0;
-     }
-   if (count == 0)
-     {
-        printk(KERN_ALERT "Do nothing");
-        return count;
-     }
-
-   count = len + 1;
-   err = copy_to_user(buf, data, count);
-   *offp = count;
-
-   if (err)
-     {
-        printk(KERN_INFO "Error in copying data");
-     }
-
-   return count;
-
+	printk( KERN_DEBUG "write handler\n");
+   //returning -1 will result into permission denied error
+	return -1;
 }
 
-ssize_t my_proc_write(struct file *filp, const char __user *buffer, size_t count, loff_t *pos)
+static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos)
 {
-   char *data = PDE_DATA(file_inode(filp));
-   if (count > DATA_SIZE)
-     return -EFAULT;
-
-   printk(KERN_ALERT "Writing to proc");
-   if (copy_from_user(data, buffer, count))
-     {
-        return -EFAULT;
-     }
-
-   data[count - 1] = '\0';
-   printk(KERN_INFO "msg has been set to %s", msg);
-
-   *pos = (int) count;
-   len = count - 1;
-
-   return count;
+	printk( KERN_DEBUG "read handler\n");
+	return 0;
 }
 
-const struct proc_ops proc_fops = {
-     .proc_read = my_proc_read,
-     .proc_write = my_proc_write,
+static struct file_operations myops =
+{
+	.owner = THIS_MODULE,
+	.read = myread,
+	.write = mywrite,
 };
 
-
-int proc_init(void)
+static int simple_init(void)
 {
-   char *DATA = "Hello World!";
-
-   msg = kmalloc((size_t) DATA_SIZE, GFP_KERNEL);
-   if (msg != NULL)
-     {
-        printk(KERN_INFO "Allocated memory for msg");
-     }
-   else return -1;
-
-   len = strlen(DATA);
-   strncpy(msg, DATA, len + 1);
-
-   proc = proc_create_data("my_proc", 0666, NULL, &proc_fops, msg);
-
-   return 0;
+	ent=proc_create("mydev",0666,NULL,&myops);
+	return 0;
 }
 
-void proc_exit(void)
+static void simple_cleanup(void)
 {
-   remove_proc_entry("my_proc", NULL);
+	proc_remove(ent);
 }
 
-MODULE_LICENSE("GPL");
+module_init(simple_init);
+module_exit(simple_cleanup)
 
-module_init(proc_init);
-module_exit(proc_exit);
