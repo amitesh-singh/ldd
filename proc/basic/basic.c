87f4181aafa8224fffcef6cbafbf1d5b2b22f6ad
Amitesh Singh <singh.amitesh@gmail.com>
2022-05-08 20:35:09 +0530

    update with latest > 5.6

--- proc/basic/basic.c
+++ proc/basic/basic.c
@@ -1,48 +1,63 @@
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/proc_fs.h>
-#include <linux/mm.h>
-#include <linux/slab.h> //kmalloc
+#include <linux/version.h>
+#include <linux/module.h>    // included for all kernel modules
+#include <linux/kernel.h>    // included for KERN_INFO
+#include <linux/init.h>      // included for __init and __exit macros
+
+#include <linux/proc_fs.h>   // file operations
+#include <linux/seq_file.h>  // seq_read, ...
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Amitesh Singh");
+MODULE_AUTHOR("Dr. Dyson");
+MODULE_DESCRIPTION("Global Information Grid");
 
-#define BUFSIZE  100
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,6,0)
+#define HAVE_PROC_OPS
+#endif
 
-static struct proc_dir_entry *ent;
+static int skynet_show(struct seq_file *m, void *v);
 
-static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos)
-{
-	printk( KERN_DEBUG "write handler\n");
-   //returning -1 will result into permission denied error
-	return -1;
-}
+static int skynet_open(struct inode *inode, struct  file *file);
 
-static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos)
-{
-	printk( KERN_DEBUG "read handler\n");
-	return 0;
-}
-
-static struct file_operations myops =
-{
-	.owner = THIS_MODULE,
-	.read = myread,
-	.write = mywrite,
+#ifdef HAVE_PROC_OPS
+static const struct proc_ops skynet_ops = {
+  .proc_open = skynet_open,
+  .proc_read = seq_read,
+  .proc_lseek = seq_lseek,
+  .proc_release = single_release,
 };
+#else
+static const struct file_operations skynet_ops = {
+  .owner = THIS_MODULE,
+  .open = skynet_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
+#endif
 
-static int simple_init(void)
-{
-	ent=proc_create("mydev",0666,NULL,&myops);
-	return 0;
+
+static int skynet_show(struct seq_file *m, void *v) {
+ here:
+  seq_printf(m, "Skynet location: 0x%lx\n", (unsigned long)&&here);
+  return 0;
 }
 
-static void simple_cleanup(void)
-{
-	proc_remove(ent);
+static int skynet_open(struct inode *inode, struct  file *file) {
+  return single_open(file, skynet_show, NULL);
 }
 
-module_init(simple_init);
-module_exit(simple_cleanup)
 
+static int __init skynet_init(void) {
+  proc_create("skynet", 0, NULL, &skynet_ops);
+  printk(KERN_INFO "Skynet in control\n");
+
+  return 0;
+}
+
+static void __exit skynet_cleanup(void) {
+  remove_proc_entry("skynet", NULL);
+  printk(KERN_INFO "I'll be back!\n");
+}
+
+module_init(skynet_init);
+module_exit(skynet_cleanup);
